c86ff3c3d105bd54d542aa144825ca24d0e07714
Johnathon Melton <seventh.acoustic@gmail.com>
2023-05-14 22:05:17 -0500

    Switch exercise done

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch(subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I dislike Math!");
+                    break;
+                case "biology":
+                    Console.WriteLine("I love Biology!");
+                    break;
+                case "history":
+                    Console.WriteLine("I enjoy history.");
+                    break;
+                case "english":
+                    Console.WriteLine("English is okay.");
+                    break;
+                case "physics":
+                    Console.WriteLine("Physics isn't my favorite.");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject");
+                    break;
+            }
         }
     }
 }
